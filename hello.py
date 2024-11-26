12af95974ab7293d6b8118bdb92c3dc17f2b6b2b
thiswind <thiswind@gmail.com>
2024-11-26 19:30:11 +0800

    添加了一个calc_add函数

--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
 
+def calc_add(a, b):
+    return a + b
+
 
 # helloworld
 if __name__ == '__main__':
     print('Hello World!')
+
+    a = 10
+    b = 20
+    c = calc_add(a, b)
+
+    print('%d + %d = %d' % (a, b, c))
